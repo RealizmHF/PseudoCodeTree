38973a96114cc732efb5c57c6bd15ba5c45cf3bb
James West <james.west2@maine.edu>
2019-08-16 06:25:06 -0400

    v1.1
    + Checks for files.txt
    + Creates and writes to files.txt
    + GUI shows files to choose from

--- src/io/github/RealizmHF/PseudoCodeTree/Driver.java
+++ src/io/github/RealizmHF/PseudoCodeTree/Driver.java
@@ -9,24 +9,34 @@ import javax.swing.JFrame;
 import javax.swing.JComponent;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
  
-public class Driver extends JPanel {
+public class Driver extends JPanel implements ListSelectionListener{
 	
 	private JTabbedPane tabs;
 	
 	private JPanel tabFile;
-	private JPanel tab2;
+	private JPanel tabTree;
 
-    public Driver() {
+    public Driver() throws IOException {
         super(new GridLayout(1, 1));
          
         tabs = new JTabbedPane();
         
-        JComponent panel1 = makeTabFile("Panel #1");
+        JComponent panel1 = makeTabFile();
         panel1.setPreferredSize(new Dimension(500, 200));
         tabs.addTab("File", panel1);
         tabs.setMnemonicAt(0, KeyEvent.VK_1);
@@ -34,7 +44,7 @@ public class Driver extends JPanel {
         
         
         JComponent panel2 = makeTab2("Panel #2");
-        tabs.addTab("Tab 2", panel2);
+        tabs.addTab("Tree", panel2);
         tabs.setMnemonicAt(1, KeyEvent.VK_2);
          
         
@@ -43,23 +53,70 @@ public class Driver extends JPanel {
         tabs.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
     }
 
-    protected JComponent makeTabFile(String text) {
+    protected JComponent makeTabFile() throws IOException {
         tabFile = new JPanel(false);
-        JList list = new JList();
+        JList<String> list = new JList<String>(createFiles());
         tabFile.setLayout(new GridLayout(1, 1));
         tabFile.add(list);
         return tabFile;
     }
     
-    protected JComponent makeTab2(String text) {
-        tab2 = new JPanel(false);
-        JTextArea filler = new JTextArea(text);
-        tab2.setLayout(new GridLayout(1, 1));
-        tab2.add(filler);
-        return tab2;
+    @Override
+    public void valueChanged(ListSelectionEvent e) 
+    { 
+        //set the text of the label to the selected value of lists
+          
     }
     
-    private static void createAndShowGUI() {
+    protected JComponent makeTab2(String text) {
+        tabTree = new JPanel(false);
+        JTextArea filler = new JTextArea(text);
+        tabTree.setLayout(new GridLayout(1, 1));
+        tabTree.add(filler);
+        return tabTree;
+    }
+    
+    private String[] createFiles() throws IOException {
+    	//Reads all files created to display a list
+    	
+    	File file = new File("files.txt");
+    	ArrayList<String> names = new ArrayList<String>();
+    	
+    	if (file.createNewFile()){
+            System.out.println("File is created!");
+            
+            //Creates a new file based on files.txt directory with the /files.txt removed
+            File fl = new File(file.getAbsolutePath().substring(0,file.getAbsolutePath().lastIndexOf(File.separator)));
+            File[] matchingFiles = fl.listFiles(new FilenameFilter() {
+                public boolean accept(File dir, String name) {
+                	//Accepts all .txt files except the files.txt file
+                    return !name.startsWith("files") && name.endsWith("txt");
+                }
+            });
+            
+            FileWriter writer = new FileWriter(file);
+            for(int k = 0; k < matchingFiles.length; k++) {
+            	writer.write(matchingFiles[k].getName() + "\n");
+            }
+            writer.close();
+            
+        }
+    	else {
+            System.out.println("File already exists.");
+        }
+
+    	Scanner scan = new Scanner(file);
+    	
+    	while(scan.hasNext()) {
+    		names.add(scan.next());
+    	}
+    	
+    	scan.close();
+    	
+    	return names.toArray(new String[names.size()]);
+    }
+    
+    private static void createAndShowGUI() throws IOException {
         //Create and set up the window.
         JFrame frame = new JFrame("Psuedo Code Tree by James West");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -79,7 +136,11 @@ public class Driver extends JPanel {
             public void run() {
                 //Turn off metal's use of bold fonts
         UIManager.put("swing.boldMetal", Boolean.FALSE);
-        createAndShowGUI();
+        try {
+			createAndShowGUI();
+		} catch (IOException e) {
+			System.out.println("createAndShowGUI Function Failed");
+		}
             }
         });
     }
